1a579d7cb53389ee277319a42d0ccd31783c2b4a
lsxxh <3477079189@qq.com>
2023-01-07 12:05:28 +0800

    解CustomNumberPicker(draw版)背景全白,发现是重写AppCompatActivity #onCreate错误,应该override fun onCreate(savedInstanceState: Bundle?)(1参)而非2参onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?)

--- app/src/main/java/com/webabcd/androiddemo/view/custom/CustomNumberPickerDemo.kt
+++ app/src/main/java/com/webabcd/androiddemo/view/custom/CustomNumberPickerDemo.kt
@@ -7,8 +7,12 @@ import com.webabcd.androiddemo.R
 
 class CustomNumberPickerDemo : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+//    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+//        super.onCreate(savedInstanceState, persistentState)
+//        setContentView(R.layout.demo_custom_numberpicker)
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.demo_custom_numberpicker)
     }
 }
